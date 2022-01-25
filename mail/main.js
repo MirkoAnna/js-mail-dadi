188368bdbe9ab115364b751da35b87c554e667d4
Mirko <mirkoanna137@gmail.com>
2022-01-25 15:03:02 +0100

    Esercizio mail (Fine)

--- mail/main.js
+++ mail/main.js
@@ -1,3 +1,15 @@
-/* Chiedi all’utente la sua email,
-controlla che sia nella lista di chi può accedere,
-stampa un messaggio appropriato sull’esito del controllo.*/
+const listaEmail = ["pippo@outlook.it", "paperino@yahoo.it", "pluto@gmail.com", "peppapig@tiscali.it"];
+
+//Chiedi all’utente la sua email
+const emailInserita = prompt("Inserisci email");
+
+// Controlla che sia nella lista di chi può accedere
+for(let i = 0; i < listaEmail.length; i ++) {
+
+    if (listaEmail.includes(emailInserita)) {
+// Stampa un messaggio appropriato sull’esito del controllo. 
+        console.log("Email valida");
+    } else {
+        console.log("Email non valida")
+    }
+}
